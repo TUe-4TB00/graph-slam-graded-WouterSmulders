cf3c56bef9cf2e4feb674ddd04346ab9bbfdab0f
Wouter Smulders <w.f.smulders@student.tue.nl>
2026-05-27 11:59:33 +0200

    assignment

--- src/add_landmark_measurement.py
+++ src/add_landmark_measurement.py
@@ -2,6 +2,7 @@ import math
 import numpy as np
 import gtsam
 from gtsam.symbol_shorthand import L, X
+from helperfunctions import add_landmark_measurement_from_global
 
 PRIOR_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.1, 0.1, 0.05]))  # (x, y, theta)
 ODOMETRY_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.2, 0.2, 0.1]))  # (dx, dy, dtheta)
@@ -9,7 +10,17 @@ MEASUREMENT_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.05, 0.1]))  # (
 
 def add_landmark_measurement(graph, initial_estimate, result):
     # Determine the correct rotation (bearing) and distance from X(4) to L(2) 
-    # rotation = 
-    # distance = 
-    graph.add(gtsam.BearingRangeFactor2D(X(4), L(2), gtsam.Rot2.fromDegrees(rotation), distance, MEASUREMENT_NOISE))
+    pose4 = result.atPose2(X(4))
+    landmark2 = result.atPoint2(L(2))
+
+    graph = add_landmark_measurement_from_global(
+        graph,
+        pose_key = X(4),
+        pose = pose4,
+        landmark_key = L(2),
+        landmark_point = landmark2,
+        measurement_noise = MEASUREMENT_NOISE,
+        add_factor = True
+    )
+    
     return graph
--- src/add_pose.py
+++ src/add_pose.py
@@ -1,8 +1,8 @@
-
 import math
 import numpy as np
 import gtsam
 from gtsam.symbol_shorthand import L, X
+from helperfunctions import add_pose_from_global
 
 PRIOR_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.1, 0.1, 0.05]))  # (x, y, theta)
 ODOMETRY_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.2, 0.2, 0.1]))  # (dx, dy, dtheta)
@@ -10,7 +10,18 @@ MEASUREMENT_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.05, 0.1]))  # (
 
 def add_pose(graph, initial_estimate):
     # TODO: Add the odometry factor between X(4) and X(5) to the graph (BetweenFactorPose2)
+    x4 = gtsam.Pose2(4.0 + math.sqrt(2), math.sqrt(2), math.pi / 2)
 
     # TODO: Based on the odometry, find the initial estimate for the pose of X(5) and add it to the graph
-    
+    prev_pose = initial_estimate.atPose2(X(3))
+
+    graph, initial_estimate = add_pose_from_global(
+        graph,
+        initial_estimate,
+        prev_key=X(3),
+        new_key=X(4),
+        prev_pose=prev_pose,
+        new_pose_global=x4,
+        odom_noise=ODOMETRY_NOISE
+    )
     return graph, initial_estimate
